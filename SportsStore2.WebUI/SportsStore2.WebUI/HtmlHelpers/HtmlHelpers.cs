b03ccfb8dfdfd23abaf3c39dba1f552ff5cc288f
nenad <nenad@DESKTOP-EL2GKTS>
2017-05-17 21:14:44 +0200

    Type: feat
    Add paging Html helper.
    Register relevant namespace with Views.

--- SportsStore2.WebUI/SportsStore2.WebUI/HtmlHelpers/HtmlHelpers.cs
+++ SportsStore2.WebUI/SportsStore2.WebUI/HtmlHelpers/HtmlHelpers.cs
@@ -1,30 +1,23 @@
 ﻿using SportsStore2.WebUI.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SportsStore2.WebUI.HtmlHelpers {
-    public static class HtmlExtensions {
+    public static class HtmlHelpers {
         public static MvcHtmlString PageLinks(this HtmlHelper htmlHelper, PagingInfo pagingInfo, Func<int, string> link) {
             StringBuilder sb = new StringBuilder();
-            TagBuilder tag = new TagBuilder("a");
+            TagBuilder tag;
             for (int i = 1; i <= pagingInfo.NrOfPages; i++) {
+                tag = new TagBuilder("a");
                 if (pagingInfo.CurrentPage == i) {
                     tag.AddCssClass("selected");
                 }
-                // kad će se pozivati ova metoda:
-                // PageLinks(pagingInfo, x => new UrlHelper().Action("List", "Product", new { page = x })
                 tag.MergeAttribute("href", link(i));
-                // Linija iznad je ista stvar kao da si i ovo ispod pozvao direktno.
-                //tag.MergeAttribute("href", new UrlHelper().Action("List", "Product", new { page = i }));
                 tag.InnerHtml = i.ToString();
                 sb.Append(tag);
             }
-
-            return null;
+            return MvcHtmlString.Create(sb.ToString());
         }
     }
 }
