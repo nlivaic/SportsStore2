098242b0438edb7113dfd0aee586430a53a5e4c0
nenad <nenad@DESKTOP-EL2GKTS>
2017-05-17 20:26:49 +0200

    Type: feat
    Add List view model.
    Define a new UI model: Paging Info (supporting class for PageLinks helper method).

---
+++ SportsStore2.WebUI/SportsStore2.WebUI/Models/ListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using SportsStore2.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore2.WebUI.Models {
+    public class ListViewModel {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
