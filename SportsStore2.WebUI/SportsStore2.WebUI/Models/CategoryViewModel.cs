32296520e51d1fb66915da3d383ded8954793e22
nenad <nenad@DESKTOP-EL2GKTS>
2017-05-23 04:41:25 +0200

    Type: feat
    List categories: view, view model.

---
+++ SportsStore2.WebUI/SportsStore2.WebUI/Models/CategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore2.WebUI.Models {
+    public class CategoryViewModel {
+        public IEnumerable<string> Categories { get; set; }
+        public string ChosenCategory { get; set; }
+    }
+}
