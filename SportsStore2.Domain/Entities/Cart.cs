869caf51cd2e575c305010162ae5bc4c6007966a
nenad <nenad@DESKTOP-EL2GKTS>
2017-05-24 22:08:03 +0200

    Type: refactor
    Cart domain entity should not have a reference to a repository. Removed all references.

--- SportsStore2.Domain/Entities/Cart.cs
+++ SportsStore2.Domain/Entities/Cart.cs
@@ -6,46 +6,36 @@ using System.Web;
 
 namespace SportsStore2.Domain.Entities {
     public class Cart {
-        public List<CartItem> Items;
-        private IProductRepository repository;
+        public IEnumerable<CartItem> Items { get { return CartItems; } }
+        private List<CartItem> CartItems;
 
-        public Cart(IProductRepository repo) {
-            Items = new List<CartItem>();
-            repository = repo;
+        public Cart() {
+            CartItems = new List<CartItem>();
         }
 
-        public void AddToCart(int productId) {
-            CartItem item = Items.Where(c => c.Item.ProductId == productId).FirstOrDefault();
-            Product product = null;
+        public void AddToCart(Product product) {
+            CartItem item = CartItems.Where(c => c.Item.ProductId == product.ProductId).FirstOrDefault();
             // If item already exists, simply advance the counter.
             if (item != null) {
                 item.Quantity++;
             } else {        // Item doesn't exists, we have to add it to the collection.
-                // But first we have to check if the product exists in the repository.
-                product = repository.Products.Where(p => p.ProductId == productId).FirstOrDefault();
-                if (product != null) {
-                    Items.Add(new CartItem {
-                        Item = product,
-                        Quantity = 1
-                    });
-                }
+                CartItems.Add(new CartItem {
+                    Item = product,
+                    Quantity = 1
+                });
             }
         }
 
-        public void RemoveFromCart(int productId) {
-            CartItem item = Items.Where(p => p.Item.ProductId == productId).FirstOrDefault();
-            // We can only remove items that were in the cart in the first place.
-            if (item != null) {
-                Items.Remove(item);
-            }
+        public void RemoveFromCart(Product product) {
+            CartItems.RemoveAll(i => i.Item.ProductId == product.ProductId);
         }
         
         public void Clear() {
-            Items.Clear();
+            CartItems.Clear();
         }
 
         public decimal GetTotalValue() {
-            return Items.Sum(p => p.Item.Price * p.Quantity);
+            return CartItems.Sum(p => p.Item.Price * p.Quantity);
         }
     }
 }
