7d3f262b19f7d6ddbe8cdbb3c4b5e9d231e0a4fd
nenad <nenad@DESKTOP-EL2GKTS>
2017-05-27 09:36:23 +0200

    Type: feat
    Order Processor interface.

---
+++ SportsStore2.Domain/Abstract/IOrderProcessor.cs
@@ -0,0 +1,7 @@
+﻿using SportsStore2.Domain.Entities;
+
+namespace SportsStore2.Domain.Abstract {
+    interface IOrderProcessor {
+        void ProcessOrder(Cart cart, ShippingDetails shippingDetails);
+    }
+}
