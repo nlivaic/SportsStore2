4578115f45359259c1863c564b36e52e295b4491
nenad <nenad@DESKTOP-EL2GKTS>
2017-05-28 20:09:36 +0200

    Type: feat
    List and Delete actions added.

--- SportsStore2.WebUI/SportsStore2.WebUI/Controllers/AdminController.cs
+++ SportsStore2.WebUI/SportsStore2.WebUI/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SportsStore2.Domain.Abstract;
+using SportsStore2.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,25 @@ namespace SportsStore2.WebUI.Controllers
 {
     public class AdminController : Controller
     {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo) {
+            repository = repo;
+        }
+
         // GET: Admin
-        public ActionResult Index()
-        {
-            return View();
+        public ViewResult List() {
+            return View(repository.Products);
+        }
+
+        // GET: Admin
+        public RedirectToRouteResult Delete(int productId) {
+            Product product = repository.Products.Where(p => p.ProductId == productId).FirstOrDefault();
+            if (product != null) {
+                //repository.DeleteProduct(product);
+                TempData["message"] = String.Format("Product {0} removed.", product.Name);
+            }
+            return RedirectToAction("List");
         }
     }
 }
