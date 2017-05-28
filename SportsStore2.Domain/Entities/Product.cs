075bbd316e3878f9413fc578bc0829ef6d560895
nenad <nenad@DESKTOP-EL2GKTS>
2017-05-28 21:43:58 +0200

    Type: feat
    Product domain model - property annotations.

--- SportsStore2.Domain/Entities/Product.cs
+++ SportsStore2.Domain/Entities/Product.cs
@@ -1,9 +1,19 @@
-﻿namespace SportsStore2.Domain.Entities {
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore2.Domain.Entities {
     public class Product {
+        [ReadOnly(true)]
         public int ProductId { get; set; }
+        [Required(ErrorMessage = "Please enter product name.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter product description.")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Please enter product category.")]
         public string Category { get; set; }
+        [Required(ErrorMessage = "Please enter product price.")]
+        [Range(0.00, double.MaxValue, ErrorMessage = "Please enter a valid price amount.")]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
     }
 }
