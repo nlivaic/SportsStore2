42fc62b42ad3886606bd95f5f730fc1596b38bca
nenad <nenad@DESKTOP-EL2GKTS>
2017-05-29 22:26:16 +0200

    Type: refactor
    Remove unused Usings.

--- SportsStore2.Domain/Concrete/EFProductRepository.cs
+++ SportsStore2.Domain/Concrete/EFProductRepository.cs
@@ -1,8 +1,5 @@
 ﻿using SportsStore2.Domain.Abstract;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using SportsStore2.Domain.Entities;
 
 namespace SportsStore2.Domain.Concrete {
