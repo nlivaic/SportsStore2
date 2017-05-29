27d489a283e6c3c2b3917a238c25d0f191aff396
nenad <nenad@DESKTOP-EL2GKTS>
2017-05-29 23:18:24 +0200

    Type: feat chore
    Implement Authorization.
    Register file with project.

---
+++ SportsStore2.Domain/Concrete/FormsAuthorization.cs
@@ -0,0 +1,9 @@
+﻿using SportsStore2.Domain.Abstract;
+
+namespace SportsStore2.Domain.Concrete {
+    public class FormsAuthorization : IAuthorize {
+        public bool Authorize(string username, string password) {
+            return true;
+        }
+    }
+}
